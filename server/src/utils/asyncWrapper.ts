92ce091a0daab85c151a3560d904ee5fb98d0443
Fawaz v <fawazchemmad@gmail.com>
2024-08-31 18:31:53 +0530

    create asyncWrapper

---
+++ server/src/utils/asyncWrapper.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+const asyncWrapper = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+export default asyncWrapper;
