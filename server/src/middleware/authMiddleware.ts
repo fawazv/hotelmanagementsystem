ba60a2752766fb87c05ff6d7419a7ab61efd7cdf
Fawaz v <fawazchemmad@gmail.com>
2024-09-02 15:02:03 +0530

    Created middleware for jwt authentication

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,34 @@
+import jwt, { Jwt, JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export interface CustomJwtPayload extends JwtPayload {
+  id: string;
+}
+
+export interface CustomRequest extends Request {
+  user?: CustomJwtPayload;
+}
+
+const authMiddleware = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+  try {
+    const decoded = jwt.verify(
+      token as string,
+      process.env.JWT_SECRET
+    ) as CustomJwtPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+export default authMiddleware;
