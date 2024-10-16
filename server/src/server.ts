fb163bcd09fa29cae89150087f8d81ce348b298b
Fawaz v <fawazchemmad@gmail.com>
2024-10-16 17:15:55 +0530

    added middleware to use cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import env from "./utils/validateEnv";
 import express from "express";
 import connectDB from "./config/db";
 import authRoute from "./routes/authRoutes";
+import errorHandler from "./middleware/errorHandler";
+import cors from "cors";
 
 const app = express();
 const port = env.PORT;
@@ -11,9 +13,13 @@ const port = env.PORT;
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 // Use user routes
 app.use("/api/auth", authRoute);
+
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Connected to backend!");
