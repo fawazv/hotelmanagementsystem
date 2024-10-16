d0567bcb82c9814f33d51fa4c880f21c06f66011
Fawaz v <fawazchemmad@gmail.com>
2024-10-16 17:13:32 +0530

    setted the prototype explicitly to maintain instance of functionality

--- server/src/utils/CustomError.ts
+++ server/src/utils/CustomError.ts
@@ -4,6 +4,8 @@ class CustomError extends Error {
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
+    // Set the prototype explicitly to maintain instanceof functionality
+    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
 
